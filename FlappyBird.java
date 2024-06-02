810467efd467d30f1c0bccad295588a78fd4440a
Seanachan <hsinchen.pai1218@gmail.com>
2024-06-02 16:26:08 +0800

    created gradle project, with one class named "snake"

--- FlappyBird.java
+++
@@ -1,5 +0,0 @@
-public class FlappyBird{
-    public static void main(String[] args){
-        
-    }
-}
---
+++ app/src/main/java/com/snake/Main.java
@@ -0,0 +1,6 @@
+package com.snake;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
