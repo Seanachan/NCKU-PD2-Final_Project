7d0d1a2113f574d9ac63f44cbfaab027537cf954
Seanachan <hsinchen.pai1218@gmail.com>
2024-06-05 23:09:35 +0800

    delete files

--- app/src/main/java/com/mario/Mario.java
+++ app/src/main/java/com/mario/Mario.java
@@ -1,10 +1,4 @@
 package com.mario;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import javax.swing.*;
 
 public class Mario extends JPanel {
