3fd256a7f07ffe46f2ca35e255ef0ad7fab6a49f
JKaiWang <a0988347904@gmail.com>
2024-06-08 18:52:52 +0800

    Enemy

--- Jkai/src/model/enemy/Enemy.java
+++ Jkai/src/model/enemy/Enemy.java
@@ -1,5 +1,15 @@
 package model.enemy;
 
-public class Enemy {
-    
+import model.GameObject;
+
+import java.awt.image.BufferedImage;
+
+
+public abstract class Enemy extends GameObject{
+
+    public Enemy(double x, double y, BufferedImage style) {
+        super(x, y, style);
+        setFalling(false);
+        setJumping(false);
+    }
 }
