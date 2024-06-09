cb8f9c551f9eaf03773d19018a71b1eac3a09a33
JKaiWang <a0988347904@gmail.com>
2024-06-09 10:56:49 +0800

    change pictur file to Fugure, define some constant and ste keyboardinput but therse are still a lot of bugs inside the project

--- Jkai/src/model/prize/Prize.java
+++ Jkai/src/model/prize/Prize.java
@@ -1,5 +1,14 @@
 package model.prize;
 
+import java.awt.Rectangle;
+import manager.MapManager;
+import manager.GameEngine;
+import model.hero.Mario;
+
 public class Prize {
     
+    int getPoint();
+    void reveal();
+    Rectangle getBounds();
+    void onTouch(Mario mario , GameEngine engine);
 }
