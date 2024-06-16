7381fa157a04ab8f6dcddf5c953a799867d7e828
kuanyi0226 <kuanyi0226@gmail.com>
2024-06-16 16:35:49 +0800

    fix test

---
+++ app/src/test/java/super_mario_nes_main/App.java
@@ -0,0 +1,10 @@
+package super_mario_nes_main;
+
+public class App {
+
+    public Object getGreeting() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGreeting'");
+    }
+
+}
